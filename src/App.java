25ff46c6832c1e886121146a71c48e9fabb7fa05
abhiramt <abhiramdevt2024@gmail.com>
2024-07-15 17:34:46 +0530

    kvbl

--- src/App.java
+++ src/App.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+
+        Scanner sc =new Scanner(System.in);
+
+        System.out.println("enter the current active count : ");
+
+        int active_count = sc.nextInt(); 
+
+        System.out.println("enter the target incriment:  ");
+        int target_inc = sc.nextInt();
+
+        System.out.println("enter the final Target : ");
+        int final_target = sc.nextInt();
+
+        int total_stage = 10 ;
+
+        int[] stage= new int [total_stage] ;
+
+        for(int i=0; i<total_stage; i++){
+           stage[i] = sc.nextInt((final_target-active_count)/i+1);
+            
+        }
+        
+        System.out.println("your each stages target are : ");
+        
+        for(int i=0; i <total_stage; i++)
+      { 
+        System.out.print(stage[i]);
+      }
+
+
+
+
+
+
     }
 }
